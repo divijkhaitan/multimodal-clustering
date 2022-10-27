c3c40f5ef6e7a3046f33bc248fb515b8009789cd
divijkhaitan <69809016+divijkhaitan@users.noreply.github.com>
2022-10-27 22:19:28 +0530

    Implement joint visualisation

---
+++ clustering.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+import pandas as pd
+from sklearn.decomposition import PCA
+from Jvis import JTSNEBASE, JTSNE
+from sklearn.cluster import KMeans
+
+expr_mat = pd.read_csv("./GSE126074_CellLineMixture_SNAREseq_cDNA_counts.tsv", sep="\t")
+
+scale_factor= 1000
+print(expr_mat)
+expr_mat = expr_mat.values

50c986dc7f833f894fe161a4599ec560c8c8a709
divijkhaitan <69809016+divijkhaitan@users.noreply.github.com>
2022-10-28 11:17:33 +0530

    Finish implementation

--- clustering.py
+++ clustering.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 import pandas as pd
 from sklearn.decomposition import PCA
-from Jvis import JTSNEBASE, JTSNE
+#from Jvis import JTSNEBASE, JTSNE
 from sklearn.cluster import KMeans
 
 expr_mat = pd.read_csv("./GSE126074_CellLineMixture_SNAREseq_cDNA_counts.tsv", sep="\t")
